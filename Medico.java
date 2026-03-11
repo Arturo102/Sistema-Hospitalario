1a2a371b8cab515b44c01fb7c15d0411c399266d
sofia lopez <lsmunoz@unimayor.edu.co>
2026-03-10 21:16:31 -0500

    feat: Sofia Lopez agrego Medico.java

---
+++ Medico.java
@@ -0,0 +1,24 @@
+public class Medico {
+
+    private int id;
+    private String nombre;
+    private String especialidad;
+
+    public Medico(int id, String nombre, String especialidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+}
